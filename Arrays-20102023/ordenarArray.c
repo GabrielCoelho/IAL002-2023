033c56d5a732ad3fb999fdc1e806ada489dd2610
Gabriel Coelho Soares <gbcoe@outlook.com.br>
2023-10-27 19:09:39 -0300

    Last changes › Still not the printing format that I've wanted

--- Arrays-20102023/ordenarArray.c
+++ Arrays-20102023/ordenarArray.c
@@ -4,10 +4,9 @@
 #include <time.h>
 
 int main(int argc, char *argv[]) {
-  int vetor[5];
   srand(time(NULL));
   printf("IREMOS ORDENAR O SEGUINTE VETOR\n");
-  int vetor_ordenado[5], organizador, menor = 0;
+  int vetor[5], vetor_ordenado[5], organizador, menor = 0;
   for (int i = 0; i < (sizeof vetor / sizeof vetor[0]); i++) {
     vetor[i] = (rand() % (25)) + 1;
     printf("[%d] -", vetor[i]);
@@ -17,7 +16,7 @@ int main(int argc, char *argv[]) {
   }
 
   for (int i = 0; i < (sizeof vetor / sizeof vetor[0]); i++) {
-    for (int j = 0; j < 5 - 1 - i; j++) {
+    for (int j = 0; j < (sizeof vetor / sizeof vetor[0]) - 1 - i; j++) {
       if (vetor_ordenado[j] > vetor_ordenado[j + 1]) {
         organizador = vetor_ordenado[j];
         vetor_ordenado[j] = vetor_ordenado[j + 1];
