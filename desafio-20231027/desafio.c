805a6e67d55bcad74c4bd0d123d515548b4e739a
Gabriel Coelho Soares <gbcoe@outlook.com.br>
2023-11-10 08:32:45 -0300

    Insert and Show array already done (but not tested)

--- desafio-20231027/desafio.c
+++ desafio-20231027/desafio.c
@@ -11,12 +11,20 @@ char menuMessage() {
   printf("\n3. Consultar no vetor");
   printf("\n4. Ordenar o vetor (ascendente)");
   printf("\n5. Sair do programa");
+  return 0;
 }
 
 int main(int argc, char *argv[]) {
   bool exit_menu = false, array_isnot_null = false;
-  int menu_option, array_position = 1;
+  int menu_option, array_position = 1, sub_menu_search_option;
   int array[5];
+
+  /* Inserindo valores "vazios" nas posições do vetor para caso o usuário queira
+   * exibir antes de inserir todos os valores.
+   */
+  for (int i = 0; i < (sizeof array / sizeof array[0]); i++) {
+    array[i] = 0;
+  }
 
   while (!exit_menu) {
     menuMessage();
@@ -38,8 +46,30 @@ int main(int argc, char *argv[]) {
       }
     /* Exibir o vetor */
     case 2:
+      if (array_isnot_null) {
+        printf("Exibindo o vetor de 5 posições das quais você já inseriu: %d "
+               "números\n",
+               array_position > 5 ? array_position : array_position - 1);
+        for (int i = 0; i < 5; i++) {
+          printf("Vetor[%d]: %d\n", i + 1, array[i]);
+        }
+      } else {
+        printf("Você ainda não inseriu nada no vetor. Por favor, passe pela "
+               "opção de inserir\n");
+      }
     /* Consultar no vetor */
     case 3:
+      if (array_isnot_null) {
+        printf("Escolha uma opção:\n1. Pesquisa por número\n2. Pesquisa por "
+               "posição\nOu qualquer outro número para voltar ao menu "
+               "principal\n\n");
+        scanf("%d", &sub_menu_search_option);
+        switch (sub_menu_search_option) {
+        case 1:
+        case 2:
+        default:
+        }
+      }
     /* Ordenar o vetor */
     case 4:
     /* sair do programa */
