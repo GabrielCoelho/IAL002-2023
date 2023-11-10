a22cb541e217e9c3785e205a0097dfbc6e3ed6ea
Gabriel Coelho Soares <gbcoe@outlook.com.br>
2023-11-10 09:09:00 -0300

    Done without testing.
    Did all the logic without testing any part of it. Next steps is to test
    and fix possible bugs and/or errors

--- desafio-20231027/desafio.c
+++ desafio-20231027/desafio.c
@@ -15,8 +15,10 @@ char menuMessage() {
 }
 
 int main(int argc, char *argv[]) {
-  bool exit_menu = false, array_isnot_null = false;
-  int menu_option, array_position = 1, sub_menu_search_option;
+  bool exit_menu = false, array_isnot_null = false,
+       found_searched_number = false;
+  int menu_option, array_position = 1, sub_menu_option, search,
+                   found_searched_position, tmp_organizer;
   int array[5];
 
   /* Inserindo valores "vazios" nas posições do vetor para caso o usuário queira
@@ -44,6 +46,7 @@ int main(int argc, char *argv[]) {
         array_position++;
         array_isnot_null = true;
       }
+      break;
     /* Exibir o vetor */
     case 2:
       if (array_isnot_null) {
@@ -57,23 +60,81 @@ int main(int argc, char *argv[]) {
         printf("Você ainda não inseriu nada no vetor. Por favor, passe pela "
                "opção de inserir\n");
       }
+      break;
     /* Consultar no vetor */
     case 3:
       if (array_isnot_null) {
         printf("Escolha uma opção:\n1. Pesquisa por número\n2. Pesquisa por "
                "posição\nOu qualquer outro número para voltar ao menu "
                "principal\n\n");
-        scanf("%d", &sub_menu_search_option);
-        switch (sub_menu_search_option) {
+        scanf("%d", &sub_menu_option);
+        switch (sub_menu_option) {
         case 1:
+          printf("Digite um número para pesquisar no Vetor: ");
+          scanf("%d", &search);
+          for (int i = 0; i < (sizeof array / sizeof array[0]); i++) {
+            found_searched_position = i + 1;
+            if (array[i] == search) {
+              found_searched_number = true;
+              break;
+            }
+          }
+          if (found_searched_number) {
+            printf("\nO número %d foi encontrado no vetor na posição %d",
+                   search, found_searched_position);
+          } else {
+            printf("\nO número %d não foi encontrado no vetor", search);
+          }
+          break;
         case 2:
+          printf("Digite uma posição para retornarmos o valor existente "
+                 "nela: ");
+          scanf("%d", &search);
+          if (search > 5 || search < 1) {
+            printf("O Vetor tem posições de 1 a 5, por favor, insira um número "
+                   "dentre estes\n");
+          } else {
+            printf("A posição %d escolhida possui o seguinte valor:\n "
+                   "Vetor[%d]: %d",
+                   search, search, array[search - 1]);
+          }
+          break;
         default:
+          printf("Saindo deste sub-menu e retornando ao menu principal");
+          break;
         }
+      } else {
+        printf("O vetor está vazio, não sendo possível pesquisar nele\n\n");
       }
     /* Ordenar o vetor */
     case 4:
+      if (array_isnot_null) {
+        for (int i = 0; i < (sizeof array / sizeof array[0]); i++) {
+          for (int j = 0; j < (sizeof array / sizeof array[0]) - 1 - i; j++) {
+            if (array[j] > array[j + 1]) {
+              tmp_organizer = array[j];
+              array[j] = array[j + 1];
+              array[j + 1] = tmp_organizer;
+            }
+          }
+        }
+        printf("O vetor foi ordenado! Deseja exibir?\n1. Sim\nOu qualquer "
+               "outro número para voltar ao menu principal");
+        scanf("%d", &sub_menu_option);
+        if (sub_menu_option != 1) {
+          break;
+        } else {
+          for (int i = 0; i < (sizeof array / sizeof array[0]); i++) {
+            printf("Vetor[%d]: %d\n", i + 1, array[i]);
+          }
+          break;
+        }
+      }
     /* sair do programa */
     case 5:
+      printf("Saindo do programa...\n\n");
+      exit_menu = true;
+      break;
     /* Outros números digitados */
     default:
       printf("Por favor, digite um número que seja acessível no menu\n\n");
