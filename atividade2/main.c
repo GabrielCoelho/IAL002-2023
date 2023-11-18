9a6436a71b0126eb420975773a24f0a54808d068
Gabriel Coelho Soares <gbcoe@outlook.com.br>
2023-11-18 14:11:55 -0300

    Menu de agência funcionando perfeitamente

--- atividade2/main.c
+++ atividade2/main.c
@@ -28,33 +28,59 @@ struct Cliente {
 
 int main(int argc, char *argv[]) {
   struct Cliente *clientes;
-  int agencia, num_clientes, iterador_cod = 1;
+  int agencia = 0, num_clientes, iterador_cod = 1;
   FILE *fro;
   FILE *fwr;
 
   inicioBanco();
-  printf("\n\nDigite o número da agência que você deseja controlar: ");
-  scanf("%d", &agencia);
+  while (agencia == 0) {
+    printf("\n\nDigite o número da agência que você deseja controlar: ");
+    scanf("%d", &agencia);
 
-  switch (agencia) {
-  case 123:
-    printf("Você escolheu a agência 123:");
+    switch (agencia) {
+    case 123:
+      printf("Você escolheu a agência de Mogi Guaçu (123)");
+      fro = fopen("123.txt", "r");
+      fwr = fopen("123.txt", "wb");
+      break;
+    case 125:
+      printf("Você escolheu a agência de Mogi Mirim (125)");
+      fro = fopen("125.txt", "r");
+      fwr = fopen("125.txt", "wb");
+      break;
+    case 129:
+      printf("Você escolheu a agência de Itapira (129)");
+      fro = fopen("129.txt", "r");
+      fwr = fopen("129.txt", "wb");
+      break;
+    case 130:
+      printf("Você escolheu a agência de Estiva Gerbi (130)");
+      fro = fopen("130.txt", "r");
+      fwr = fopen("130.txt", "wb");
+      break;
+    default:
+      printf("Você não selecionou nenhuma agência existente!\nPor favor, "
+             "selecione corretamente!");
+      agencia = 0;
+      break;
+    }
   }
   // Procurar pelo arquivo de mesmo nome. Caso não exista, criar nova agência
   // Checar quantidade de linhas no arquivo para gerar o número de clientes a
   // partir da seguinte fórmula: num_clientes = (50-nlinhas) + nlinhas;
 
-  printf("Digite a quantidade de clientes cadastrados nessa agência: ");
-  scanf("%d", &num_clientes);
-  // Alocando um espaço de memória de acordo com o número de clientes
-  clientes = (struct Cliente *)malloc(num_clientes * sizeof(struct Cliente));
-
-  printf("Cadastro de Cliente número");
-  for (iterador_cod; iterador_cod <= num_clientes; iterador_cod++) {
-    printf("Digite o nome do cliente e em seguida o número da conta corrente "
-           "com o dígito: ");
-    scanf("%s %s", &(clientes + (iterador_cod - 1))->nome_cliente,
-          &(clientes + (iterador_cod - 1))->conta_corrente);
-  }
+  // printf("Digite a quantidade de clientes cadastrados nessa agência: ");
+  // scanf("%d", &num_clientes);
+  // // Alocando um espaço de memória de acordo com o número de clientes
+  // clientes = (struct Cliente *)malloc(num_clientes * sizeof(struct Cliente));
+  //
+  // printf("Cadastro de Cliente número");
+  // for (iterador_cod; iterador_cod <= num_clientes; iterador_cod++) {
+  //   printf("Digite o nome do cliente e em seguida o número da conta corrente
+  //   "
+  //          "com o dígito: ");
+  //   scanf("%s %s", &(clientes + (iterador_cod - 1))->nome_cliente,
+  //         &(clientes + (iterador_cod - 1))->conta_corrente);
+  // }
   return EXIT_SUCCESS;
 }
